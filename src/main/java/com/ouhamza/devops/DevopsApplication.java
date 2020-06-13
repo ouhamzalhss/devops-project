647ea19b5a42104ff781f5d6a3d2db24b82825f3
ouhamzalhss <ouhamza.web.pro@gmail.com>
2020-06-13 16:55:26 +0100

    add SpringBootServletInitializer in springBootApplication

--- src/main/java/com/ouhamza/devops/DevopsApplication.java
+++ src/main/java/com/ouhamza/devops/DevopsApplication.java
@@ -2,12 +2,20 @@ package com.ouhamza.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DevopsApplication {
-
+public class DevopsApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
+
 		SpringApplication.run(DevopsApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DevopsApplication.class);
+	}
 }
+
+
